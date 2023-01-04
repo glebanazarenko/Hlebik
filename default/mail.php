22f65ec4d4ce46c70385e35a104705d9e7c6f0d3
glebanazarenko <106635217+glebanazarenko@users.noreply.github.com>
2023-01-04 04:40:23 +0300

    Add sending emails
    Добавлена рассылка на почту админа(меня) и на почту, что указали в форме

---
+++ default/mail.php
@@ -0,0 +1,70 @@
+<?php
+
+$method = $_SERVER['REQUEST_METHOD'];
+
+//Script Foreach
+$c = true;
+if ( $method === 'POST' ) {
+
+	$project_name = trim($_POST["project_name"]);
+	$admin_email  = trim($_POST["admin_email"]);
+    $email  = trim($_POST["email"]);
+	$form_subject = trim($_POST["form_subject"]);
+    $fio = trim($_POST["fio"]);
+    $phone = trim($_POST["phone"]);
+    $content = trim($_POST["content"]);
+
+	
+    /*foreach ( $_POST as $key => $value ) {
+		if ( $value != "" && $key != "project_name" && $key != "admin_email" && $key != "form_subject" ) {
+			$message .= "
+			" . ( ($c = !$c) ? '<tr>':'<tr style="background-color: #f8f8f8;">' ) . "
+				<td style='padding: 10px; border: #e9e9e9 1px solid;'><b>$key</b></td>
+				<td style='padding: 10px; border: #e9e9e9 1px solid;'>$value</td>
+			</tr>
+			";
+		}
+	}*/
+
+    $message .= "Здраствуйте, ".$fio."
+    Ваше сообщение скоро будет рассмотрено
+                        С наилучшими пожеланиями, 
+                        Проект HLEBIk";
+
+    $message2 .="
+    ФИО человека ".$fio."
+    Телефон человека ".$phone."
+    Почта человека ".$email."
+    Тема сообщения ".$form_subject."
+    Текст сообщения ".$content."
+    ";
+
+} else if ( $method === 'GET' ) {
+
+	$project_name = trim($_GET["project_name"]);
+	$admin_email  = trim($_GET["admin_email"]);
+	$form_subject = trim($_GET["form_subject"]);
+
+	foreach ( $_GET as $key => $value ) {
+		if ( $value != "" && $key != "project_name" && $key != "admin_email" && $key != "form_subject" ) {
+			$message .= "
+			" . ( ($c = !$c) ? '<tr>':'<tr style="background-color: #f8f8f8;">' ) . "
+				<td style='padding: 10px; border: #e9e9e9 1px solid;'><b>$key</b></td>
+				<td style='padding: 10px; border: #e9e9e9 1px solid;'>$value</td>
+			</tr>
+			";
+		}
+	}
+}
+
+function adopt($text) {
+	return '=?UTF-8?B?'.Base64_encode($text).'?=';
+}
+
+$headers = "MIME-Version: 1.0" . PHP_EOL .
+"Content-Type: text/html; charset=utf-8" . PHP_EOL .
+'From: '.adopt($project_name).' <'.$admin_email.'>' . PHP_EOL .
+'Reply-To: '.$admin_email.'' . PHP_EOL;
+
+mail($email, adopt($form_subject), $message, $headers );
+mail("nazarenkogleb228@gmail.com", adopt($form_subject), $message2, $headers );
